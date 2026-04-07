412b0590a95d24f57bed09491cd6cabdea392833
lucia8307 <luciabigailchavezsosa@gmail.com>
2026-04-06 21:11:03 -0300

    Crea proyecto y aplicación y configura myproject/settings.py y app/models.py

---
+++ Proyecto DRF/app/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.core.validators import RegexValidator
+
+class Users(models.Model):
+    name = models.CharField(
+        max_length=100,
+        help_text="Nombre completo del usuario."
+    )
+    email = models.EmailField(
+        unique=True,
+        help_text="Correo electrónico único."
+    )
+    dni = models.CharField(
+        max_length=20,
+        unique=True,
+        validators=[RegexValidator(r'^\d{7,8}$')],
+        help_text="Número de documento (7 u 8 dígitos)."
+    )
+    birth_date= models.DateField(
+        help_text="Fecha de nacimiento."
+    )
+    role= models.CharField(
+        max_length=10,
+            choices=[('admin', 'Administrador'), ('editor', 'Editor'),
+        ('cliente', 'Cliente')],
+        default='cliente',
+        help_text="Rol del usuario."
+    )
+    activo = models.BooleanField(default=True)
+    def __str__(self):
+        return f"{self.nombre} ({self.rol})"
